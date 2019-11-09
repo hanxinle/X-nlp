ea62ea5526b4968d6f680e905f7d22af98a619aa
hanxinle <hanxloop@foxmail.com>
2019-11-09 12:46:16 +0800

    maven demo

---
+++ mavendemo.java
@@ -0,0 +1,13 @@
+package mavendemo;
+import com.hankcs.hanlp.HanLP;
+
+
+public class mavendemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.print(HanLP.segment("你好，欢迎使用我的开发包"));
+	}
+}
+
+
